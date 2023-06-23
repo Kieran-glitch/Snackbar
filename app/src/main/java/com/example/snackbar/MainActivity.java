8479666fbf50e9681abf7b8859d6e22cc3b18de3
Kieran Ameresekere <kieraniscoolerthanu@gmail.com>
2023-06-23 17:51:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/snackbar/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"this is our snackbar",Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
